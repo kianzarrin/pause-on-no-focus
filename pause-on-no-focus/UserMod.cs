8fef146a1057ac6b8af79056c2cd025bcb01d008
kianzzarrin <kian.zarrin@gmail.com>
2022-10-04 22:38:20 +0300

    added option excluded kian commons.

--- pause-on-no-focus/UserMod.cs
+++ pause-on-no-focus/UserMod.cs
@@ -1,29 +1,58 @@
 ﻿using ICities;
 using UnityEngine;
-using KianCommons;
-using KianCommons.IImplict;
+using System.Collections;
+using ColossalFramework.UI;
+using System;
 
 namespace PauseOnNoFocus
 {
+    public static class Extensions {
+        public static UICheckBox AddUpdatingCheckbox(
+        this UIHelperBase helper, string label, Action<bool> SetValue, Func<bool> GetValue)
+        {
+            Debug.Log($"option {label} is " + GetValue());
+            var cb = helper.AddCheckbox(label, GetValue(), delegate (bool value) {
+                try
+                {
+                    SetValue(value);
+                    Debug.Log($"option '{label}' is set to " + value);
+                }
+                catch (Exception ex) { Debug.LogException(ex); }
+            }) as UICheckBox;
+            cb.eventVisibilityChanged += (c, val) => (c as UICheckBox).isChecked = GetValue();
+            return cb;
+
+        }
+    }
+
     public class Mod : IUserMod
     {
+        public string Description => "Automatically pauses the game when it is not focused";
+        public string Name => "Pause on no focus";
 
-        public string Description
+        public void OnSettingsUI(UIHelper helper)
         {
-            get { return "Automatically pauses the game when it is not focused"; }
+            helper.AddUpdatingCheckbox(
+                "Run in background",
+                val => Application.runInBackground = val,
+                () => Application.runInBackground);
         }
 
-        public string Name
-        {
-            get { return "Pause on no focus"; }
-        }
+
     }
 
     public class PauseOnUnfocus : LoadingExtensionBase
     {
         public override void OnLevelLoaded(LoadMode mode)
         {
+            LoadingManager.instance.QueueLoadingAction(LoadAction());
+        }
+
+        public static IEnumerator LoadAction()
+        {
+            yield return 0;
             Application.runInBackground = false;
+            yield break;
         }
 
         public override void OnLevelUnloading()
