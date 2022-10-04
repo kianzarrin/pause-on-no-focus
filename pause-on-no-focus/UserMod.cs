d84823f7d405a1f45c1df90d50a95ef6da608528
kianzzarrin <kian.zarrin@gmail.com>
2022-10-04 22:09:07 +0300

    first commit

---
+++ pause-on-no-focus/UserMod.cs
@@ -0,0 +1,34 @@
+﻿using ICities;
+using UnityEngine;
+using KianCommons;
+using KianCommons.IImplict;
+
+namespace PauseOnNoFocus
+{
+    public class Mod : IUserMod
+    {
+
+        public string Description
+        {
+            get { return "Automatically pauses the game when it is not focused"; }
+        }
+
+        public string Name
+        {
+            get { return "Pause on no focus"; }
+        }
+    }
+
+    public class PauseOnUnfocus : LoadingExtensionBase
+    {
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            Application.runInBackground = false;
+        }
+
+        public override void OnLevelUnloading()
+        {
+            Application.runInBackground = true;
+        }
+    }
+}
